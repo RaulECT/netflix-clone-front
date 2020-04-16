840ac551c2cb59dfb042d83ce01059b0dc306522
Raul Canul <eduaroct120@gmail.com>
2020-04-16 16:19:03 -0500

    graphql file created

---
+++ src/graphql.js
@@ -0,0 +1,25 @@
+import { ApolloClient } from 'apollo-client';
+import { createUploadLink } from 'apollo-upload-client';
+import { setContext } from 'apollo-link-context';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const API_URL = process.env.LINK_BACK || 'https://netflix-clone-backend.herokuapp.com';
+
+const httpLink = createUploadLink({ uri: API_URL });
+
+const authLink = setContext((_, {headers}) => {
+  const token = sessionStorage.getItem( 'appToken' )
+  const context = {
+    headers: {
+      ...headers,
+    }
+  };
+
+  if ( token ) context.headers['authorization'] = `JWT ${token}`;
+  return context;
+});
+
+export default new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+})
