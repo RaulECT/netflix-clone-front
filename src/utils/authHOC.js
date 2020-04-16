cd777f9c7c3cd67ec735652db83a4bb489eb95c1
Raul Canul <eduaroct120@gmail.com>
2020-04-16 15:05:57 -0500

    authHOC created

---
+++ src/utils/authHOC.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+import authenticate from './authenticate';
+
+export default function( WrappedComponent ) {
+  return function( props ) {
+    const { isAuthenticate } = authenticate();
+    
+    return isAuthenticate
+      ? <WrappedComponent {...props} />
+      : <Redirect to='/auth' />
+  }
+};
