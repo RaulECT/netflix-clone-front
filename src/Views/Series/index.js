5b480bee358bcf6490710a91e89c9416c033b9ff
Raul Canul <eduaroct120@gmail.com>
2020-04-16 20:54:10 -0500

    Series connected to back-end

--- src/Views/Series/index.js
+++ src/Views/Series/index.js
@@ -1,87 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import authHOC from '../../utils/authHOC';
+import { useQuery } from 'react-apollo-hooks';
+import gql from 'graphql-tag'
+import { Skeleton } from 'antd'
 
 import VideoList from '../Components/VideosList';
 
-const videos = [
-  {
-    "_id": "5e95054545d962083dfdc14d",
-    "name": "Movie 1",
-    "image_cover_uri": "https://res.cloudinary.com/dmclfzpce/image/upload/v1586825716/mjxx8bnfxalariwd0wxg.jpg",
-    "trailer_uri": "https://res.cloudinary.com/dmclfzpce/video/upload/v1586825780/mbbqnn3oxrekqg4mpqv9.mov",
-    "rating": 0
-  },
-  {
-    "_id": "5e95054545d962083dfdc14d",
-    "name": "Movie 1",
-    "image_cover_uri": "https://res.cloudinary.com/dmclfzpce/image/upload/v1586825716/mjxx8bnfxalariwd0wxg.jpg",
-    "trailer_uri": "https://res.cloudinary.com/dmclfzpce/video/upload/v1586825780/mbbqnn3oxrekqg4mpqv9.mov",
-    "rating": 0
-  },
-  {
-    "_id": "5e95054545d962083dfdc14d",
-    "name": "Movie 1",
-    "image_cover_uri": "https://res.cloudinary.com/dmclfzpce/image/upload/v1586825716/mjxx8bnfxalariwd0wxg.jpg",
-    "trailer_uri": "https://res.cloudinary.com/dmclfzpce/video/upload/v1586825780/mbbqnn3oxrekqg4mpqv9.mov",
-    "rating": 0
-  },
-  {
-    "_id": "5e95054545d962083dfdc14d",
-    "name": "Movie 1",
-    "image_cover_uri": "https://res.cloudinary.com/dmclfzpce/image/upload/v1586825716/mjxx8bnfxalariwd0wxg.jpg",
-    "trailer_uri": "https://res.cloudinary.com/dmclfzpce/video/upload/v1586825780/mbbqnn3oxrekqg4mpqv9.mov",
-    "rating": 0
-  },
-  {
-    "_id": "5e95054545d962083dfdc14d",
-    "name": "Movie 1",
-    "image_cover_uri": "https://res.cloudinary.com/dmclfzpce/image/upload/v1586825716/mjxx8bnfxalariwd0wxg.jpg",
-    "trailer_uri": "https://res.cloudinary.com/dmclfzpce/video/upload/v1586825780/mbbqnn3oxrekqg4mpqv9.mov",
-    "rating": 0
-  },
-  {
-    "_id": "5e95054545d962083dfdc14d",
-    "name": "Movie 1",
-    "image_cover_uri": "https://res.cloudinary.com/dmclfzpce/image/upload/v1586825716/mjxx8bnfxalariwd0wxg.jpg",
-    "trailer_uri": "https://res.cloudinary.com/dmclfzpce/video/upload/v1586825780/mbbqnn3oxrekqg4mpqv9.mov",
-    "rating": 0
-  },
-  {
-    "_id": "5e95054545d962083dfdc14d",
-    "name": "Movie 1",
-    "image_cover_uri": "https://res.cloudinary.com/dmclfzpce/image/upload/v1586825716/mjxx8bnfxalariwd0wxg.jpg",
-    "trailer_uri": "https://res.cloudinary.com/dmclfzpce/video/upload/v1586825780/mbbqnn3oxrekqg4mpqv9.mov",
-    "rating": 0
-  },
-  {
-    "_id": "5e95054545d962083dfdc14d",
-    "name": "Movie 1",
-    "image_cover_uri": "https://res.cloudinary.com/dmclfzpce/image/upload/v1586825716/mjxx8bnfxalariwd0wxg.jpg",
-    "trailer_uri": "https://res.cloudinary.com/dmclfzpce/video/upload/v1586825780/mbbqnn3oxrekqg4mpqv9.mov",
-    "rating": 0
-  },
-  {
-    "_id": "5e95054545d962083dfdc14d",
-    "name": "Movie 1",
-    "image_cover_uri": "https://res.cloudinary.com/dmclfzpce/image/upload/v1586825716/mjxx8bnfxalariwd0wxg.jpg",
-    "trailer_uri": "https://res.cloudinary.com/dmclfzpce/video/upload/v1586825780/mbbqnn3oxrekqg4mpqv9.mov",
-    "rating": 0
-  },
-  {
-    "_id": "5e95054545d962083dfdc14d",
-    "name": "Movie 1",
-    "image_cover_uri": "https://res.cloudinary.com/dmclfzpce/image/upload/v1586825716/mjxx8bnfxalariwd0wxg.jpg",
-    "trailer_uri": "https://res.cloudinary.com/dmclfzpce/video/upload/v1586825780/mbbqnn3oxrekqg4mpqv9.mov",
-    "rating": 0
-  },
-]
+const SERIES = gql`
+  query getAllSeries{
+    getSeries {
+      _id
+      name
+      image_cover_uri
+    }
+  }
+`
 
 function Series() {
+  const { loading, data } = useQuery(SERIES);
+  const [series, updateSeries] = useState([]);
+
+  useEffect(() => {
+    if (data) {
+      updateSeries(data.getSeries)
+    }
+  }, [data])
 
   return (
-    <section>
-      <VideoList videos={videos} title='Series' />
-    </section>
+    <Skeleton active loading={loading}>
+      <section>
+        <VideoList videos={series} title='Series' />
+      </section>
+    </Skeleton>
   )
 }
 
