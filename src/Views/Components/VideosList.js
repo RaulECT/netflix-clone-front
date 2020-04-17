75fb865c0334d9f67bd5e5737a565371fc441f14
Raul Canul <eduaroct120@gmail.com>
2020-04-16 21:14:45 -0500

    VideoList empty videos option implemented

--- src/Views/Components/VideosList.js
+++ src/Views/Components/VideosList.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import { Divider } from 'antd'
+import { Divider, Result } from 'antd'
 
 import VideoCard from './VideoCard';
 
-function VideoList({videos, title}) {
-  const videoCards = videos.map( ({ _id, name, image_cover_uri }) => 
-    <VideoCard 
-      key={`vc__${_id}`} 
-      cover={image_cover_uri} 
-      name={name} 
-    /> );
+function VideoList({ videos, title }) {
+  const videoCards = videos.map(({ _id, name, image_cover_uri }) =>
+    <VideoCard
+      key={`vc__${_id}`}
+      cover={image_cover_uri}
+      name={name}
+    />);
 
-  return (
-    <>
-      <Divider orientation='left'>{title}</Divider>
-      <section className='video-list'>
-        { videoCards }
-      </section>
-    </>
-  );
+  return videos.length !== 0
+    ? (
+      <>
+        <Divider orientation='left'>{title}</Divider>
+        <section className='video-list'>
+          {videoCards}
+        </section>
+      </>
+    )
+    : (
+      <Result 
+        status='404'
+        title='Ups, no tenemos ninguna serie :('
+        subTitle='Por el momento no tenemos series listadas, date tu vuelta luego.'
+      />
+    )
+
+  // return (
+    
+  // );
 };
 
 export default VideoList;
