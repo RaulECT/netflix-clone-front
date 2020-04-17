320ad8b41db0aae9485560e34dac0bf885f2ce35
Raul Canul <eduaroct120@gmail.com>
2020-04-16 20:18:00 -0500

    VideoContent connected to back-end

--- src/Views/VideoContent/Components/Recomendation.js
+++ src/Views/VideoContent/Components/Recomendation.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import { Button } from 'antd'
 
-function Recomendation({ video }){
+const defaultVideo = {
+  "_id": "5e95054545d962083dfdc14d",
+  "name": "Movie 1",
+  "image_cover_uri": "https://res.cloudinary.com/dmclfzpce/image/upload/v1586825716/mjxx8bnfxalariwd0wxg.jpg",
+  "trailer_uri": "https://res.cloudinary.com/dmclfzpce/video/upload/v1586825780/mbbqnn3oxrekqg4mpqv9.mov",
+  "rating": 0
+}
+
+function Recomendation({ video = defaultVideo }){
   const { trailer_uri, image_cover_uri, name } = video;
 
   return (
--- src/Views/VideoContent/index.js
+++ src/Views/VideoContent/index.js
@@ -1,74 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import authHOC from '../../utils/authHOC';
+import { useQuery } from 'react-apollo-hooks';
+import gql from 'graphql-tag';
+import { Skeleton } from 'antd'
 
 import Recomendations from './Components/Recomendation';
 import VideosList from '../Components/VideosList';
 import './video-content.css'
 
+const MOVIES = gql`
+  query getAllVideos{
+    getVideos {
+      _id
+      name
+      image_cover_uri
+      trailer_uri
+      rating
+    }
+  }
+`
+
 const videos = [
-  {
-    "_id": "5e95054545d962083dfdc14d",
-    "name": "Movie 1",
-    "image_cover_uri": "https://res.cloudinary.com/dmclfzpce/image/upload/v1586825716/mjxx8bnfxalariwd0wxg.jpg",
-    "trailer_uri": "https://res.cloudinary.com/dmclfzpce/video/upload/v1586825780/mbbqnn3oxrekqg4mpqv9.mov",
-    "rating": 0
-  },
-  {
-    "_id": "5e95054545d962083dfdc14d",
-    "name": "Movie 1",
-    "image_cover_uri": "https://res.cloudinary.com/dmclfzpce/image/upload/v1586825716/mjxx8bnfxalariwd0wxg.jpg",
-    "trailer_uri": "https://res.cloudinary.com/dmclfzpce/video/upload/v1586825780/mbbqnn3oxrekqg4mpqv9.mov",
-    "rating": 0
-  },
-  {
-    "_id": "5e95054545d962083dfdc14d",
-    "name": "Movie 1",
-    "image_cover_uri": "https://res.cloudinary.com/dmclfzpce/image/upload/v1586825716/mjxx8bnfxalariwd0wxg.jpg",
-    "trailer_uri": "https://res.cloudinary.com/dmclfzpce/video/upload/v1586825780/mbbqnn3oxrekqg4mpqv9.mov",
-    "rating": 0
-  },
-  {
-    "_id": "5e95054545d962083dfdc14d",
-    "name": "Movie 1",
-    "image_cover_uri": "https://res.cloudinary.com/dmclfzpce/image/upload/v1586825716/mjxx8bnfxalariwd0wxg.jpg",
-    "trailer_uri": "https://res.cloudinary.com/dmclfzpce/video/upload/v1586825780/mbbqnn3oxrekqg4mpqv9.mov",
-    "rating": 0
-  },
-  {
-    "_id": "5e95054545d962083dfdc14d",
-    "name": "Movie 1",
-    "image_cover_uri": "https://res.cloudinary.com/dmclfzpce/image/upload/v1586825716/mjxx8bnfxalariwd0wxg.jpg",
-    "trailer_uri": "https://res.cloudinary.com/dmclfzpce/video/upload/v1586825780/mbbqnn3oxrekqg4mpqv9.mov",
-    "rating": 0
-  },
-  {
-    "_id": "5e95054545d962083dfdc14d",
-    "name": "Movie 1",
-    "image_cover_uri": "https://res.cloudinary.com/dmclfzpce/image/upload/v1586825716/mjxx8bnfxalariwd0wxg.jpg",
-    "trailer_uri": "https://res.cloudinary.com/dmclfzpce/video/upload/v1586825780/mbbqnn3oxrekqg4mpqv9.mov",
-    "rating": 0
-  },
-  {
-    "_id": "5e95054545d962083dfdc14d",
-    "name": "Movie 1",
-    "image_cover_uri": "https://res.cloudinary.com/dmclfzpce/image/upload/v1586825716/mjxx8bnfxalariwd0wxg.jpg",
-    "trailer_uri": "https://res.cloudinary.com/dmclfzpce/video/upload/v1586825780/mbbqnn3oxrekqg4mpqv9.mov",
-    "rating": 0
-  },
-  {
-    "_id": "5e95054545d962083dfdc14d",
-    "name": "Movie 1",
-    "image_cover_uri": "https://res.cloudinary.com/dmclfzpce/image/upload/v1586825716/mjxx8bnfxalariwd0wxg.jpg",
-    "trailer_uri": "https://res.cloudinary.com/dmclfzpce/video/upload/v1586825780/mbbqnn3oxrekqg4mpqv9.mov",
-    "rating": 0
-  },
-  {
-    "_id": "5e95054545d962083dfdc14d",
-    "name": "Movie 1",
-    "image_cover_uri": "https://res.cloudinary.com/dmclfzpce/image/upload/v1586825716/mjxx8bnfxalariwd0wxg.jpg",
-    "trailer_uri": "https://res.cloudinary.com/dmclfzpce/video/upload/v1586825780/mbbqnn3oxrekqg4mpqv9.mov",
-    "rating": 0
-  },
   {
     "_id": "5e95054545d962083dfdc14d",
     "name": "Movie 1",
@@ -79,12 +31,31 @@ const videos = [
 ]
 
 function VideoContent() {
+  const { loading, data } = useQuery(MOVIES);
+  const [ allVideos, updateVideos ] = useState([]);
+
+  useEffect( () => {
+    if ( data ) {
+      updateVideos( data.getVideos );
+    }
+  }, [data] )
+
+  function getRandomVideo() {
+    if ( allVideos.length !== 0 ) {
+      const randomIndex = Math.floor( Math.random * ( allVideos.length - 1 ) );
+      return allVideos[randomIndex];
+    }
+
+    return videos[1]
+  }
 
   return (
-    <section className='video-content'>
-      <Recomendations video={videos[1]} />
-      <VideosList videos={videos} title='Nuestro catalogo' />
-    </section>
+    <Skeleton active loading={loading}>
+      <section className='video-content'>
+        <Recomendations video={getRandomVideo()} />
+        <VideosList videos={allVideos} title='Nuestro catalogo' />
+      </section>
+    </Skeleton>
   );
 }
 
